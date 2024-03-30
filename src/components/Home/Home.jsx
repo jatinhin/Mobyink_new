fd212db10b603a520066ef76769582c017e0c376
jatin2040947 <jatin@mobyink.com>
2024-03-30 15:58:57 +0530

    ASdADSA

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+
+const Home = () => {
+  return (
+    <Stack width={"100%"} height={"100%"}>
+    </Stack>
+  )
+}
+
+export default Home
