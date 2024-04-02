c8ab7e32a0cce29246145434b1a9375c6627873d
Anshu <imanshu822@gmail.com>
2024-04-02 09:50:05 +0530

    completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
-<<<<<<< HEAD
 import React from "react";
 import { Stack } from "@mui/material";
 import Slider from "../../utils/Slider";
 import TabLineSection from "./parts/TabLineSection";
 import Steps from "./parts/Steps";
 import ShappingTheFuture from "./parts/ShappingTheFuture";
-import CardSection from "./parts/CardSection";
 import SharkTankClients from "./parts/SharkTankClients";
+import ScrollingImages from "./parts/ScrollingImages";
+import ThreeSection from "./parts/ThreeSection";
+import CardSection from "./parts/ScrollingImages";
 const Home = () => {
   return (
     <Stack width={"100%"} height={"100%"} bgcolor={"#F5F7FF"}>
@@ -14,25 +15,11 @@ const Home = () => {
       <Slider />
       <Steps />
       <ShappingTheFuture />
-      <CardSection />
       <SharkTankClients />
+      <ThreeSection />
+      <CardSection />
     </Stack>
   );
 };
-=======
-import React from 'react'
-import ScrollingImages from './parts/ScrollingImages'
-import ThreeSection from './parts/ThreeSection'
-import CardSection from './parts/ScrollingImages'
-
-const Home = () => {
-  return (
-    <>
-      <ThreeSection />
-      <CardSection />
-    </>
-  );
-}
->>>>>>> main
 
 export default Home;
