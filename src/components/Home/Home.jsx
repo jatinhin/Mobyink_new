b68553783408ca42988e5fa9ef6550df6f085e9a
jatin2040947 <jatin@mobyink.com>
2024-04-02 12:47:18 +0530

    asd

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -18,20 +18,6 @@ const Home = () => {
     </Stack>
   );
 };
-=======
-import React from 'react'
-import ScrollingImages from './parts/ScrollingImages'
-import ThreeSection from './parts/ThreeSection'
-import CardSection from './parts/ScrollingImages'
 
-const Home = () => {
-  return (
-    <>
-      <ThreeSection />
-      <CardSection />
-    </>
-  );
-}
->>>>>>> main
 
 export default Home;
